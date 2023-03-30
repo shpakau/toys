05672777ea5a8cb5c3557dac731e4ead628334f8
shpakau <shpakau@yahoo.com>
2023-03-30 13:48:16 +0300

    added ability to add: toys and its characteristics

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,44 @@
+import java.util.Scanner;
 import java.io.IOException;
-import java.util.Random;
-
 public class Main {
-    public static void main(String[] args) {
-        try {
-            Toy toy = new Toy();
-            Toy selectedToy = toy.play();
-            if (selectedToy != null) {
-                System.out.println("Вы выиграли игрушку: " + selectedToy.getName());
-            } else {
-                System.out.println("К сожалению, никто не выиграл призовую игрушку.");
-            }
-        } catch (IOException e) {
-            System.out.println("Ошибка при чтении файла: " + e.getMessage());
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        Toy toy = new Toy();
+
+        System.out.println("1 - Добавьте новую игрушку");
+        System.out.println("2 - Изменение веса игрушки");
+        System.out.println("3 - Попробуешь выиграть?");
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("Введите ID игрушки:");
+                int id = scanner.nextInt();
+                System.out.println("Напишите название игрушки:");
+                String name = scanner.next();
+                System.out.println("Введите количество игрушек:");
+                int quantity = scanner.nextInt();
+                System.out.println("Введите вес игрушки:");
+                double weight = scanner.nextDouble();
+                toy.addToy(id, name, quantity, weight);
+                break;
+
+            case 2:
+                System.out.println("Введите ID игрушки:");
+                int toyId = scanner.nextInt();
+                System.out.println("Введите новый вес игрушки:");
+                double newWeight = scanner.nextDouble();
+                toy.changeWeight(toyId, newWeight);
+                break;
+
+            case 3:
+                Toy prize = toy.play();
+                System.out.println("Поздравляю! Вы выиграли " + prize.getName() + "!");
+                break;
+
+            default:
+                System.out.println("Неправильно.");
+                break;
         }
     }
 }
